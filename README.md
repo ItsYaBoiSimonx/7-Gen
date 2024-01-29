e21c916d0c6dec589ae700c966669038ef0d21d6
simonx <simone.savarese2402@gmail.com>
2024-01-30 00:24:10 +0100

    created repo

---
+++ README.md
@@ -0,0 +1,36 @@
+<h1 align="center">🎇 7-gen, a tool made to exploit guilded's bad account management system</h1>
+
+<p align="center">
+    <img alt="funny badge" src="https://forthebadge.com/images/badges/made-with-python.svg">
+    <img alt="funny badge" src="https://forthebadge.com/images/badges/open-source.svg">
+</p>
+
+To run this program as intended, you'll need the newest version of python AND PIP downloaded off the official [python.org](https://www.python.org/downloads) website (both python and pip [added to path](https://www.youtube.com/watch?v=3a3UOSBffUI)) to run this correctly.
+
+This tool is fully made in Python, it uses a hardcoded HTTPX client to create 7 accounts at the same time.
+
+<h1 align="center">- USAGE -</h1>
+
+Direct releases will be out very soon™, but as of right now, you can just ```git clone``` the repository and manually run it.
+
+```
+$ git clone https://github.com/ItsYaBoiSimonx/7-gen
+```
+
+* If you find ANY issues with this program after you've tried it yourself, don't be afraid to make a pull request or dm me on telegram, @StraightSW 🎉
+
+
+<h1 align="center">- TODO -</h1>
+* Add proxy support, desperately.
+* Add an account manager (since, after you're done with the accounts, they're gonna be useless)
+* "Humanizer" option (since, as of right now, the names are just random characters)
+
+<p align="center">
+  <img src="images/Oke4iqJrpa.png"/>
+</p>
+
+<h3 align="center">
+    ⌛ Just as an FYI, I am not responsible for anything you do with this project ⌛
+    <br>
+    Have fun with it, just not too much.
+</h3>
---
+++ main.py
@@ -0,0 +1,154 @@
+import httpx,json,string, threading, requests, random
+from random import choice,choices
+import time 
+from uuid import uuid4
+from datetime import datetime
+from pystyle import Colorate, Center, Colors, Write
+import os
+from colorama import Fore, Style, Back
+
+
+def set_console_window_size(width, height):
+    os.system(f"mode con: cols={width} lines={height}")
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def generate_email():
+    username_length = random.randint(5, 10)
+    domain_length = random.randint(5, 8)
+
+    username = ''.join(random.choice(string.ascii_lowercase) for _ in range(username_length))
+    domain = ''.join(random.choice(string.ascii_lowercase) for _ in range(domain_length))
+
+    email = f"{username}@{domain}.com"
+    return email
+
+def warning(message):
+    timestamp = datetime.now().strftime('%H:%M:%S')
+    print(f"{Fore.LIGHTBLACK_EX}{timestamp} {Fore.YELLOW}? {Style.RESET_ALL}{message}")
+
+def success(message):
+    timestamp = datetime.now().strftime('%H:%M:%S')
+    print(f"{Fore.LIGHTBLACK_EX}{timestamp} {Fore.GREEN}+ {Style.RESET_ALL}{message}")
+
+def error(message):
+    timestamp = datetime.now().strftime('%H:%M:%S')
+    print(f"{Fore.LIGHTBLACK_EX}{timestamp} {Fore.RED}! {Style.RESET_ALL}{message}")
+
+def formatCookies(items: dict) -> str:
+    cookies_text = str()
+    for cookie in items: cookies_text += f'{cookie[0]}={cookie[1]}; '
+    success('formatted the cookies successfully')
+    return cookies_text[:len(cookies_text)-2]
+
+proxy = open('proxies.txt','r',encoding='utf-8').read().splitlines()
+
+def joinServer(session: httpx.Client, code):
+   success('starting the joinServer function')
+   session.headers['content-length'] = "19"
+   
+   r = session.put(url=f'https://www.guilded.gg/api/invites/{code}', data=json.dumps({"type":"consume"}))
+   
+   if r.status_code == 200:
+       success("Invite code joined")
+   else:
+       warning('Joining server')
+
+os.system('cls' if os.name == 'nt' else 'clear')
+set_console_window_size(90,25)
+
+print(Colorate.Vertical(Colors.white_to_green, Center.XCenter("""
+                                         
+                    ██████████                             
+                   ░███░░░░███                             
+                   ░░░    ███   ███████  ██████  ████████  
+                         ███   ███░░███ ███░░███░░███░░███ 
+                        ███   ░███ ░███░███████  ░███ ░███ 
+                       ███    ░███ ░███░███░░░   ░███ ░███ 
+                      ███     ░░███████░░██████  ████ █████
+                     ░░░       ░░░░░███ ░░░░░░  ░░░░ ░░░░░ 
+                               ███ ░███                    
+                              ░░██████                     
+                               ░░░░░░                      
+                                                                  
+            ⌜―――――――――――――――――――――――――――――――――――――――――――――――――――――⌝
+            ┇      [Github]  https://github.com/ItsYaBoiSimonx    ┇                                                                  
+            ┇             guilded is very well made               ┇
+            ⌞―――――――――――――――――――――――――――――――――――――――――――――――――――――⌟ 
+                                                            
+                                                            """, 2)))
+
+preInvite = input(Fore.LIGHTGREEN_EX + "Enter invite link - ")
+fullInv = preInvite.split('/i/')[1]
+channelid = input(Fore.LIGHTGREEN_EX + "Enter channel ID - ")
+message = input(Fore.LIGHTGREEN_EX + "Enter message - ")
+
+def spamMessages(session: httpx.Client, channnelID, message, name):
+    while True:
+        try:
+            payload = {"messageId": str(uuid4()),"content":{"object":"value","document":{"object":"document","data":{},"nodes":[{"object":"block","type":"paragraph","data":{},"nodes":[{"object":"text","leaves":[{"object":"leaf","text":f"{message} | get botted lmfao","marks":[]}]}]}]}},"repliesToIds":[],"confirmed":False,"isSilent":False,"isPrivate":False}
+            dumpedPayload = json.dumps(payload)
+            session.headers['content-length'] = str(len(dumpedPayload))
+            res = session.post(
+                f"https://www.guilded.gg/api/channels/{channnelID}/messages",
+                json=payload
+            )
+            success(f"Sent message as {name}")
+            time.sleep(1)
+        except Exception as e:
+            error(str(e)) 
+    
+        
+def create(invite):
+    global proxy, message, channelid
+    while True:
+        email = generate_email()
+        password = generate_password()
+        proxytouse = random.choice(proxy)
+        alphanumeric = string.ascii_letters + string.digits
+        proxyStruct = {
+            "http://": f"http://{proxytouse}",
+            "https://": f"http://{proxytouse}"
+        }
+        username = ''.join(random.choice(alphanumeric) for _ in range(10))
+        session = httpx.Client()
+        payload = {"extraInfo":{"platform":"desktop"},
+                "name": username,
+                "email": email,
+                "password": password,
+                "fullName": username,
+        }
+
+        try:
+            r = session.post(url='https://www.guilded.gg/api/users?type=email', json=payload, timeout=3.5)
+            success('Posted the login payload successfully')
+        except Exception as e:
+            error(f'ERROR! {e}')
+        try:
+            if "email" in r.text:
+                with open('accounts.txt', 'a') as f:
+                    f.write(f"{username}:{email}:{password}:{session.cookies['hmac_signed_session']}\n")
+            else:
+                error('Failed to generate account with email.')
+                time.sleep(5)
+        except Exception as e:
+            error(e)
+
+        if r.status_code == 200:
+            id = r.json()['user']['id']
+            success(f"{username} created")
+            joinServer(session, invite)
+            spamMessages(session, channelid, message, username)
+            time.sleep(2)
+
+
+num_threads = 7
+threads = []
+
+for i in range(num_threads):
+    thread = threading.Thread(target=create, args=(fullInv,))
+    threads.append(thread)
+    thread.start()
