913a1163f971d0c06f1d8c40ad683cf76151e46a
simonx <simone.savarese2402@gmail.com>
2024-01-30 00:29:54 +0100

    added comments

--- main.py
+++ main.py
@@ -46,17 +46,6 @@ def formatCookies(items: dict) -> str:
 
 proxy = open('proxies.txt','r',encoding='utf-8').read().splitlines()
 
-def joinServer(session: httpx.Client, code):
-   success('starting the joinServer function')
-   session.headers['content-length'] = "19"
-   
-   r = session.put(url=f'https://www.guilded.gg/api/invites/{code}', data=json.dumps({"type":"consume"}))
-   
-   if r.status_code == 200:
-       success("Invite code joined")
-   else:
-       warning('Joining server')
-
 os.system('cls' if os.name == 'nt' else 'clear')
 set_console_window_size(90,25)
 
@@ -86,6 +75,18 @@ fullInv = preInvite.split('/i/')[1]
 channelid = input(Fore.LIGHTGREEN_EX + "Enter channel ID - ")
 message = input(Fore.LIGHTGREEN_EX + "Enter message - ")
 
+
+def joinServer(session: httpx.Client, code):
+   success('starting the joinServer function')
+   session.headers['content-length'] = "19"
+   
+   r = session.put(url=f'https://www.guilded.gg/api/invites/{code}', data=json.dumps({"type":"consume"}))
+   
+   if r.status_code == 200:
+       success("Invite code joined")
+   else:
+       warning('Joining server')
+
 def spamMessages(session: httpx.Client, channnelID, message, name):
     while True:
         try:
@@ -103,16 +104,27 @@ def spamMessages(session: httpx.Client, channnelID, message, name):
     
         
 def create(invite):
+    """
+
+    Starts the creation process for the account.
+    Uses HTTPX for the requests.
+    PROXY SUPPORT IS NOT ADDED YET.
+
+    """
+
     global proxy, message, channelid
     while True:
         email = generate_email()
         password = generate_password()
         proxytouse = random.choice(proxy)
         alphanumeric = string.ascii_letters + string.digits
-        proxyStruct = {
-            "http://": f"http://{proxytouse}",
-            "https://": f"http://{proxytouse}"
-        }
+        # proxyStruct = {
+        #     "http://": f"http://{proxytouse}",
+        #     "https://": f"http://{proxytouse}"
+        # }
+
+        # im too lazy to account for every proxy error so i just commented it out till i can manage.
+
         username = ''.join(random.choice(alphanumeric) for _ in range(10))
         session = httpx.Client()
         payload = {"extraInfo":{"platform":"desktop"},
